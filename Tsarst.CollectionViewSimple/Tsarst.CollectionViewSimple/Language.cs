b8389a1736780dbd161013fb6d6542067562ea29
talistalis <tsarst@gmail.com>
2022-11-22 21:57:55 +0300

    Добавьте файлы проекта.

---
+++ Tsarst.CollectionViewSimple/Tsarst.CollectionViewSimple/Language.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tsarst.CollectionViewSimple
+{
+    /// <summary>
+    /// Класс описывающий язык программирования
+    /// </summary>
+    public class Language
+    {
+        public string Name { get; set; } // имя языка программирования
+
+        public string Description { get; set; } // описание языка программирования
+
+        public string Image { get; set; } // ссылка на лого языка программирования
+    }
+}
---
+++ Tsarst.CollectionViewSimple/Tsarst.CollectionViewSimple/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tsarst.CollectionViewSimple
+{
+    public partial class MainPage : ContentPage
+    {
+        public IList<Language> Languages { get; set; } // коллекция в которой хранятся языки программирования
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            Languages = new List<Language>(); // создается экземпляр Листа
+
+            Languages.Add(new Language()
+            {
+
+            });
+
+            base.OnAppearing();
+        }
+    }
+}
